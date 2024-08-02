ade6f27cc112364a44af70663c5acae3816dc5e5
Eduarda Donato <eduardaribeiro0202@hotmail.com>
2024-08-02 12:22:13 -0300

    start struct project

---
+++ src/main/java/br/com/laserIot/palletManager/PalletManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.laserIot.palletManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalletManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalletManagerApplication.class, args);
+	}
+
+}
